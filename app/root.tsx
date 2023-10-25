6fd8bd6cbd93ee0b38d28ff4a03f7cd9ec5c2124
Faisal-Mahmud-Anik <bandafaisalmahmud@gmail.com>
2023-10-25 21:52:14 +0600

    created  dynamic contact route page with static data

--- app/root.tsx
+++ app/root.tsx
@@ -1,8 +1,11 @@
 import { LinksFunction } from "@remix-run/node";
 import {
+  Form,
   Links,
   LiveReload,
   Meta,
+  NavLink,
+  Outlet,
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
@@ -24,8 +27,39 @@ export default function App() {
         <Links />
       </head>
       <body>
-        <h1>Hello world!!!</h1>
-        <div id="details"></div>
+        <div id="sidebar">
+          <h1>Remix Contacts</h1>
+          <div>
+            <Form id="search-form" role="search">
+              <input
+                type="search"
+                aria-label="Search contacts"
+                id="q"
+                name="q"
+                placeholder="search"
+              />
+              <div aria-hidden hidden={true} id="search spinner" />
+            </Form>
+            <Form method="post">
+              <button type="submit">New</button>
+            </Form>
+          </div>
+          <nav>
+            <ul>
+              <li>
+                <NavLink to={`contacts/${1}`}>Your Name</NavLink>
+              </li>
+              <li>
+                <NavLink to={`contacts/${2}`}>Your Friend</NavLink>
+              </li>
+            </ul>
+          </nav>
+        </div>
+
+        <div id="details">
+          <Outlet />
+        </div>
+
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
