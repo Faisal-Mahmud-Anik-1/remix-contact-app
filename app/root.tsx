62354a4b9c82908f24665550b5f79fd414579ed9
Faisal-Mahmud-Anik <bandafaisalmahmud@gmail.com>
2023-10-25 20:58:04 +0600

    Remove boilerplate code and create own root.tsx file

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,4 @@
 import {
-  Form,
   Links,
   LiveReload,
   Meta,
@@ -9,43 +8,16 @@ import {
 
 export default function App() {
   return (
-    <html lang="en">
-      <head>
+    <html>
+      <head lang="en">
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
       </head>
       <body>
-        <div id="sidebar">
-          <h1>Remix Contacts</h1>
-          <div>
-            <Form id="search-form" role="search">
-              <input
-                id="q"
-                aria-label="Search contacts"
-                placeholder="Search"
-                type="search"
-                name="q"
-              />
-              <div id="search-spinner" aria-hidden hidden={true} />
-            </Form>
-            <Form method="post">
-              <button type="submit">New</button>
-            </Form>
-          </div>
-          <nav>
-            <ul>
-              <li>
-                <a href={`/contacts/1`}>Your Name</a>
-              </li>
-              <li>
-                <a href={`/contacts/2`}>Your Friend</a>
-              </li>
-            </ul>
-          </nav>
-        </div>
-
+        <h1>Hello world!!!</h1>
+        <div id="details"></div>
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
